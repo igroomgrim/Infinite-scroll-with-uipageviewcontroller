462de579851d94cd8c7ace73f664c709aebe90c2
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-10 19:19:18 +0700

    initial Xcode project (swift)

---
+++ Infinite-scroll-swift/Infinite-scroll-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infinite-scroll-swift
+//
+//  Created by Anak Mirasing on 5/10/16.
+//  Copyright © 2016 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
